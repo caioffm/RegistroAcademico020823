d3643b3ba5ab6004e0a17d2b78b47f5366cab8b1
caioffm <ridan696@gmail.com>
2023-08-02 21:30:10 -0300

    Registro Academico 02/08/23

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Aluno aluno1 = new Aluno("RA12345", "Caio felipe", 1.0, 7.5, 9.0);
+        Aluno aluno1 = new Aluno("RA12345", "Caio Felipe", 8.0, 7.5, 9.0);
         aluno1.imprimeResultado();
+        Aluno aluno2 = new Aluno("RA12345", "Christian Moscão", 1.0, 7.5, 9.0);
+        aluno2.imprimeResultado();
     }
 }
